980546bc47f39cd5730966e53c1f3a7e080ccf01
unknown <p_cohen@TGG-PC022.GreatestGood.com>
2015-04-06 08:33:25 -0500

    committing before major refactor of baseline_models

--- Modeling code.py
+++ Modeling code.py
@@ -8,6 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 import pandas as pd
 import numpy as np
+import random
 from functools import partial
 import os
 sys.path.append("C:/Git_repos/ProductClasses")
@@ -15,11 +16,34 @@ import matplotlib.pyplot as plt
 import data_describe as dd
 
 ############################ Functions ##################################
-def stratified_in_out_samp(df, strat_var, split_rt):
+def prep_data(data_path, name, is_train=1):
+    """ Prepare data for Kaggle Competition """
+    # load data
+    df = pd.read_csv(data_path + name)
+    # create validation if it is train data
+    if is_train:
+        df = stratified_in_out_samp(df, 'target', .75)
+    # recode target as numeric
+    if is_train:
+        df['target_num'] = df['target'].map(lambda x: x[-1:]).astype(int)
+    return df
+    
+def list_feats(df, non_feats):
+    """ Creates list of all modeling features """
+    # Create list of all columns
+    Xfeats = df.columns.values.tolist()
+    # remove non modeling columns from feature list
+    for col in non_feats:
+        Xfeats.remove(col)
+    return Xfeats
+
+def stratified_in_out_samp(df, strat_var, split_rt, seed=42):
     """ 
     This function splits raw data into a train and validation sample,
     that is stratified on the selected variable
     """
+    # set seed
+    random.seed(seed)
     # Group data by strat var
     grouped = df.groupby(df[strat_var])
     # create a frame to store is_val values
@@ -78,14 +102,12 @@ def baseline_models(df, feats, target='target'):
     trn = df[df.is_val == 0].reset_index()
     val = df[df.is_val == 1].reset_index()
     # Define forests to test
-    forests = {'forst1' : [3000, None],
-               'forst2' : [30, 25],
+    forests = {'forst1' : [3000, None], 'forst2' : [30, 25],
                'forst3' : [30, 12]}
-    forests = {'forst1' : [30, None],
-               'forst2' : [30, 25],
-               'forst3' : [30, 12]}
+    forests = {'forst3' : [30, 12], 'forst2' : [200, 25]}
+    
     # initialize best forest score
-    best_frst = 10000
+    best_frst = {'model' : 'none', 'score' : 10000, 'preds' : 'none'}
     # fit and evaluate each forest
     for name, params in forests.iteritems():
         # define forest
@@ -99,19 +121,17 @@ def baseline_models(df, feats, target='target'):
         score = multiclass_log_loss(val[target]-1, preds)
         print str(name) + " has a score of " + str(score)
         # store the predictions of the best forest run
-        if score < best_frst:
-            best_frst = score
-            best_frst_preds = preds
+        if score < best_frst['score']:
+            best_frst = {'model' : frst, 'score' : score, 'preds' : preds}
     # initialize best boost score
-    best_boost = 10000
+    best_boost = {'model' : 'none', 'score' : 10000, 'preds' : 'none'}
     # define boosted trees to try
-    boosts = {'boost1' : [300, 3, .11],
-              'boost2' : [300, 3, .13],
-              'boost3' : [300, 3, .14],
-              'boost4' : [1200, 1, .07],
-              'boost5' : [1200, 1, .08],
-              'boost6' : [1200, 1, .09]}
-              
+    boosts = {'boost1' : [300, 3, .135], 'boost2' : [300, 3, .14],
+              'boost3' : [300, 3, .145],
+              'boost4' : [1200, 1, .1], 'boost5' : [1200, 1, .11],
+              'boost6' : [1200, 1, .14]
+              }
+    boosts = {'boost1' : [3, 3, .135]} 
     # fit and test each boost specification
     for name, params in boosts.iteritems():
         # create boost specification
@@ -126,14 +146,57 @@ def baseline_models(df, feats, target='target'):
         score = multiclass_log_loss(val[target]-1, preds)
         print str(name) + " has a score of " + str(score)
         # store the predictions of the best boost run
-        if score < best_boost:
-            best_boost = score
-            best_boost_preds = preds
-    blend = best_boost_preds + best_frst_preds
-    score = multiclass_log_loss(val[target]-1,  blend)
-    print "Blended score is: " + str(score)
-     
-            
+        if score < best_boost['score']:
+            best_boost = {'model' : frst, 'score' : score, 'preds' : preds}
+    # create list of best models
+    best_models = [best_frst, best_boost]    
+    # determine sum of scores from best models
+    best_score = 100
+    for mod in best_models:
+        best_score = min(best_score, mod['score'])
+    # create scaled weights for each model (arbitrary combination)
+    for mod in best_models:
+        mod['weight'] = 1 / ((mod['score'] - best_score + .1)*10)
+        print mod['weight']
+    # create a blended score
+    blend_preds = 0
+    for mod in best_models:
+        blend_preds += mod['weight']*mod['preds']
+    blend_score = multiclass_log_loss(val[target]-1, blend_preds)
+    print "Blended score is: " + str(blend_score)
+    return best_models
+  
+def create_subm(models, df_test, feats, subm_path, nm):
+    """ 
+    Creates a kaggle submission from models, assumes Kaggle rescaling
+    
+    Inputs: 
+    
+    models - dictionary with models to use, (predictions and weights optional)
+    
+    df - test data
+    
+    feats - features to predict on
+    
+    subm_path - path to folder storing submissions
+    
+    nm - name of submission
+    """
+    # load sample submission
+    sample_sub = pd.read_csv(subm_path + 'sampleSubmission.csv')
+    # reset all non id columns to zero
+    sample_sub.ix[:, 1:] = 0
+    # create blended predictions
+    for mod in models:
+        # isolate test features
+        testX = df_test[feats]
+        # create weight predictions
+        scaled_preds = mod['weight']*mod['model'].predict_proba(testX)
+        # Create weighted average of models in sample sub
+        sample_sub.ix[:, 1:] += scaled_preds
+    sample_sub.ix[:, 0] = sample_sub.ix[:, 0].astype(int)
+    # export submission
+    sample_sub.to_csv(subm_path + nm + '.csv', index=False)      
  
 ############################ Tests ##################################
 def test_strat_samp():
@@ -156,15 +219,13 @@ def test_strat_samp():
     print('Function stratifies sample correctly')
 
 ########################### Live Code ###################################
-PATH = "S:/03 Internal - Current/Kaggle/Otto Group Product Classification"
-PATH2 = "/Structured Data/05 Data Documentation/"
+PATH = "S:/03 Internal - Current/Kaggle/Otto Group Product Classification/Structured Data/"
+PATH2 = "05 Data Documentation/"
+SUBM_PATH = "03 Final Datasets/Submissions/"
 OUTPATH = PATH + PATH2
-df = pd.read_csv(PATH + "/Structured Data/01 Raw Datasets/train.csv")
 
-df_wval = stratified_in_out_samp(df, 'target', .75)
-df_wval['target_num'] = df_wval['target'].map(lambda x: x[-1:]).astype(int)
-Xfeats = df_wval.columns.values.tolist()
-non_feats = ['id', 'target', 'is_val', 'target_num']
-for col in non_feats:
-    Xfeats.remove(col)
-baseline_models(df_wval, Xfeats, target='target_num')
+df_train = prep_data(PATH+PATH2+"../01 Raw Datasets/", "train.csv")
+df_test = prep_data(PATH+PATH2+"../01 Raw Datasets/", "test.csv", is_train=0)
+Xfeats = list_feats(df_train, ['id', 'target', 'is_val', 'target_num'])  
+best_models = baseline_models(df_train, Xfeats, target='target_num')
+create_subm(best_models, df_test, Xfeats, PATH + SUBM_PATH, 'testing')
